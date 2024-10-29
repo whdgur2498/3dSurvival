3f9bd43a1df1aac4d8ba1628b687a99ab92e7fac
whdgur2498 <whdgur2498@gmail.com>
2024-10-29 11:27:42 +0900

    [Add]Condition
    1.UIcondition
    2.condition
    3.PlayerCondition
    [Update]Player.cs

--- Assets/Script/UI/Condition.cs
+++ Assets/Script/UI/Condition.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Condition : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float curValue;
+    public float startValue;
+    public float maxValue;
+    public float passiveValue;
+    public Image uiBar;
+
+
     void Start()
     {
-        
+        curValue = startValue;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        uiBar.fillAmount = GetPercentage();
+    }
+
+    float GetPercentage()
+    { 
+        return curValue / maxValue; 
+    }
+
+    public void Add(float value)
+    {
+        curValue = Mathf.Min(curValue + value, maxValue);
+    }
+
+    public void Subtract(float value)
+    { 
+        curValue = Mathf.Max(curValue - value, 0);
     }
 }
--- Assets/Script/UI/UICondition.cs
+++ Assets/Script/UI/UICondition.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class UICondition : MonoBehaviour
 {
+    public Condition health;
+    public Condition hunger;
+    public Condition stamina;
+
     // Start is called before the first frame update
     void Start()
     {
